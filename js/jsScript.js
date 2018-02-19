ee6915c14da1397c774b22dbad173d7e09ca7006
website-layout-hho <developer777@i.ua>
2018-02-19 06:24:28 +0200

    adaptiv

--- js/jsScript.js
+++ js/jsScript.js
@@ -1,8 +1,20 @@
- var header = new Headhesive('.hedaer_menu');
+ var options = {
+     offset:500
+ };
+var header = new Headhesive('.hedaer_menu,options');
 
  $('a[data-target="anchor"]').bind('click.smoothscroll',function(){
      var target = $(this).attr('href'),
-         bl_top = $(target).offset().top - 45;
-     $('body,html').animate({scrollTop: bl_top}, 1700);
+         bl_top = $(target).offset().top - 55;
+     $('body,html').animate({scrollTop: bl_top}, 1000);
      return false;
  })
+ function myFunction (){
+var x=document.getElementById("myTopnav");
+if( x.className === "menu"){
+    x.className +=" responsive";
+}else{
+x.className ="menu";
+}
+};
+
